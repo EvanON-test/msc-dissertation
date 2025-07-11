abe56d5765f18a743047618fa1edc4fcc4c6c98c
EvanON-test <e22neale22@gmail.com>
2025-07-11 16:52:22 +0100

    GPU test. Batch size to 64. Error - GPU processing check

--- processing/binary_classifier_util.py
+++ processing/binary_classifier_util.py
@@ -33,10 +33,13 @@ def rescale_image(image):
 
 # TODO: Verify this approach works
 def rescale_image_gpu(image):
-    gpu_image.upload(image)
-    cv2.cuda.cvtColor(gpu_image, cv2.COLOR_BGR2GRAY, gpu_grey)
-    cv2.cuda.resize(gpu_grey, (LOW_RES_WIDTH, LOW_RES_HEIGHT), gpu_resized)
-    return gpu_resized.download()
+    try:
+        gpu_image.upload(image)
+        cv2.cuda.cvtColor(gpu_image, cv2.COLOR_BGR2GRAY, gpu_grey)
+        cv2.cuda.resize(gpu_grey, (LOW_RES_WIDTH, LOW_RES_HEIGHT), gpu_resized)
+        return gpu_resized.download()
+    except Exception as e:
+        print("GPU processing failed due to: " + str(e))
 
 
 # reshape in tf compatible format
