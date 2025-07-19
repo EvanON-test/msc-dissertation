3e17469825cbfecf1db8a8718c4e23b4fb4ab483
EvanON-test <e22neale22@gmail.com>
2025-07-19 15:31:48 +0100

    Real-Time: fix attempt. Most basic approach

--- realtime_pipeline.py
+++ realtime_pipeline.py
@@ -1,3 +1,4 @@
+from sys import exception
 
 import numpy as np
 import shutil
@@ -15,32 +16,44 @@ import processing.keypoint_detector_util as kd
 
 class RealtimePipeline:
     def realtime_test(self):
+        try:
+            gst_stream = ["gst-launch1.0", "nvarguscamerasrc", "!", "nvegltransform", "!", "nveglglessink" "-e" ]
+        except exception as e:
+            print("Fell at the first hurdle due to: " + str(e))
 
-        gst_stream = "nvarguscamerasrc sensor_id=0 ! \
-        'video/x-raw(memory:NVMM),width=1920, height=1080, framerate=30/1' ! \
-        nvvidconv flip-method=0 ! 'video/x-raw,width=960, height=540' ! \
-        nvvidconv ! nvegltransform ! nveglglessink -e"
-        capture = cv2.VideoCapture(gst_stream, cv2.CAP_GSTREAMER)
 
-        if (capture.isOpened() == False):
-            print("Error opening video stream or file")
-            try:
-                capture_2 = cv2.VideoCapture(0)
-            except Exception as e:
-                    print("FAILED ON BOTH ACCOUNTS, due to: " + str(e))
 
-        while capture.isOpened():
-            ret, frame = capture.read()
-            if ret == True:
-                cv2.imshow('frame', frame)
 
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
-            else:
-                break
 
-        capture.release()
-        cv2.destroyAllWindows()
+
+
+
+        # gst_stream = "nvarguscamerasrc sensor_id=0 ! \
+        # 'video/x-raw(memory:NVMM),width=1920, height=1080, framerate=30/1' ! \
+        # nvvidconv flip-method=0 ! 'video/x-raw,width=960, height=540' ! \
+        # nvvidconv ! nvegltransform ! nveglglessink -e"
+        #
+        # capture = cv2.VideoCapture(gst_stream, cv2.CAP_GSTREAMER)
+        #
+        # if (capture.isOpened() == False):
+        #     print("Error opening video stream or file")
+        #     try:
+        #         capture_2 = cv2.VideoCapture(0)
+        #     except Exception as e:
+        #             print("FAILED ON BOTH ACCOUNTS, due to: " + str(e))
+        #
+        # while capture.isOpened():
+        #     ret, frame = capture.read()
+        #     if ret == True:
+        #         cv2.imshow('frame', frame)
+        #
+        #         if cv2.waitKey(1) & 0xFF == ord('q'):
+        #             break
+        #     else:
+        #         break
+        #
+        # capture.release()
+        # cv2.destroyAllWindows()
 
 
 
