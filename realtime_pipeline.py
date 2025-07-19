d81ef19a90fab60dd789c7bedcb14a992c04bfc7
EvanON-test <e22neale22@gmail.com>
2025-07-19 14:38:37 +0100

    Real-Time: gst_stream updated

--- realtime_pipeline.py
+++ realtime_pipeline.py
@@ -11,13 +11,16 @@ import processing.frame_selector_util as fs
 import processing.object_detector_util as od
 import processing.keypoint_detector_util as kd
 
-COMPLETED_FILES_LOG = "./CompletedFiles.txt"
 
-#Updated this into a class
+
 class RealtimePipeline:
     def realtime_test(self):
-        gst_stream = "nvarguscamerasrc sensor_id=0 ! nvoverlaysink"
-        capture = cv2.VideoCapture(gst_stream)
+
+        gst_stream = "nvarguscamerasrc sensor_id=0 ! \
+        'video/x-raw(memory:NVMM),width=1920, height=1080, framerate=30/1' ! \
+        nvvidconv flip-method=0 ! 'video/x-raw,width=960, height=540' ! \
+        nvvidconv ! nvegltransform ! nveglglessink -e"
+        capture = cv2.VideoCapture(gst_stream, cv2.CAP_GSTREAMER)
 
         if (capture.isOpened() == False):
             print("Error opening video stream or file")
